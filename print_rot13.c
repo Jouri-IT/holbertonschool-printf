742e596926480f71f85f1c4c181ba31367bc7676
tala2277 <tt1341700@gmail.com>
2026-04-16 17:04:08 +0000

    fix rot13 output

--- print_rot13.c
+++ print_rot13.c
@@ -1,46 +1,48 @@
 #include "main.h"
 
 int print_rot13(va_list args, char buffer[], int *index,
- char flag, char length, int width, int precision)
+	char flag, char length, int width, int precision)
 {
- char *str;
- char *in;
- char *out;
- int len;
- int count;
- int i;
- char c;
+	char *str;
+	char *in;
+	char *out;
+	int len;
+	int count;
+	int i;
+	int j;
+	char c;
 
- (void)flag;
- (void)length;
+	(void)flag;
+	(void)length;
 
- in = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
- out = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	in = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	out = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
- str = va_arg(args, char *);
- if (!str)
-  str = "(null)";
+	str = va_arg(args, char *);
+	if (!str)
+		str = "(null)";
 
- len = 0;
- while (str[len])
-  len++;
+	len = 0;
+	while (str[len])
+		len++;
 
- if (precision >= 0 && precision < len)
-  len = precision;
+	if (precision >= 0 && precision < len)
+		len = precision;
 
- count = 0;
- count += print_padding(width, len, buffer, index);
+	count = 0;
+	count += print_padding(width, len, buffer, index);
 
- for (i = 0; i < len; i++)
- {
-  c = str[i];
-  for (i = 0; in[i] && in[i] != c; i++)
-   ;
-  if (in[i])
-   count += buffer_char(out[i], buffer, index);
-  else
-   count += buffer_char(c, buffer, index);
- }
+	for (i = 0; i < len; i++)
+	{
+		c = str[i];
+		j = 0;
+		while (in[j] && in[j] != c)
+			j++;
+		if (in[j])
+			count += buffer_char(out[j], buffer, index);
+		else
+			count += buffer_char(c, buffer, index);
+	}
 
- return (count);
+	return (count);
 }
