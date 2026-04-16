c0288ec21eccd54b4af4267217181223fe3d77d2
tala2277 <tt1341700@gmail.com>
2026-04-16 16:04:30 +0000

    add precision support

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 
 #define BUF_SIZE 1024
 
-static int (*get_handler(char c))(va_list, char[], int *, char, char, int)
+static int (*get_handler(char c))(va_list, char[], int *, char, char, int, int)
 {
 	int i;
 	specifier_t specs[] = {
@@ -11,21 +11,16 @@ static int (*get_handler(char c))(va_list, char[], int *, char, char, int)
 		{'%', print_percent},
 		{'d', print_int},
 		{'i', print_int},
-		{'b', print_binary},
 		{'u', print_unsigned},
 		{'o', print_octal},
 		{'x', print_hex_lower},
 		{'X', print_hex_upper},
-		{'S', print_S},
-		{'p', print_pointer},
 		{0, NULL}
 	};
 
 	for (i = 0; specs[i].handler; i++)
-	{
 		if (specs[i].spec == c)
 			return (specs[i].handler);
-	}
 	return (NULL);
 }
 
@@ -47,20 +42,17 @@ static int is_digit(char c)
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count;
-	int (*handler)(va_list, char[], int *, char, char, int);
+	int count = 0;
+	int (*handler)(va_list, char[], int *, char, char, int, int);
 	char buffer[BUF_SIZE];
-	int buf_index;
-	char flag;
-	char length;
-	int width;
+	int buf_index = 0;
+	char flag, length;
+	int width, precision;
 
 	if (!format)
 		return (-1);
 
 	va_start(args, format);
-	count = 0;
-	buf_index = 0;
 
 	while (*format)
 	{
@@ -74,6 +66,7 @@ int _printf(const char *format, ...)
 			flag = 0;
 			length = 0;
 			width = 0;
+			precision = -1;
 
 			while (is_flag(*format))
 			{
@@ -81,8 +74,6 @@ int _printf(const char *format, ...)
 					flag = '+';
 				else if (*format == ' ' && flag != '+')
 					flag = ' ';
-				else if (*format == '#' && flag == 0)
-					flag = '#';
 				format++;
 			}
 
@@ -95,33 +86,44 @@ int _printf(const char *format, ...)
 			if (*format == '*')
 			{
 				width = va_arg(args, int);
-				if (width < 0)
-					width = 0;
 				format++;
 			}
 			else
 			{
 				while (is_digit(*format))
 				{
-					width = (width * 10) + (*format - '0');
+					width = width * 10 + (*format - '0');
 					format++;
 				}
 			}
 
-			if (!*format)
+			if (*format == '.')
 			{
-				va_end(args);
-				return (-1);
+				format++;
+				precision = 0;
+
+				if (*format == '*')
+				{
+					precision = va_arg(args, int);
+					format++;
+				}
+				else
+				{
+					while (is_digit(*format))
+					{
+						precision = precision * 10 + (*format - '0');
+						format++;
+					}
+				}
 			}
 
 			handler = get_handler(*format);
+
 			if (handler)
-				count += handler(args, buffer, &buf_index, flag, length, width);
+				count += handler(args, buffer, &buf_index, flag, length, width, precision);
 			else
 			{
 				count += buffer_char('%', buffer, &buf_index);
-				if (width == 0 && flag == 0 && length == 0 && *format == '*')
-					count += buffer_char('*', buffer, &buf_index);
 				count += buffer_char(*format, buffer, &buf_index);
 			}
 		}
--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 typedef struct specifier_s
 {
 	char spec;
-	int (*handler)(va_list, char[], int *, char, char, int);
+	int (*handler)(va_list, char[], int *, char, char, int, int);
 } specifier_t;
 
 int _printf(const char *format, ...);
@@ -16,16 +16,13 @@ int buffer_char(char c, char buffer[], int *index);
 int flush_buffer(char buffer[], int *index);
 int print_padding(int width, int len, char buffer[], int *index);
 
-int print_char(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_string(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_percent(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_int(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_binary(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_unsigned(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_octal(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_hex_lower(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_hex_upper(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_S(va_list args, char buffer[], int *index, char flag, char length, int width);
-int print_pointer(va_list args, char buffer[], int *index, char flag, char length, int width);
+int print_char(va_list, char[], int *, char, char, int, int);
+int print_string(va_list, char[], int *, char, char, int, int);
+int print_percent(va_list, char[], int *, char, char, int, int);
+int print_int(va_list, char[], int *, char, char, int, int);
+int print_unsigned(va_list, char[], int *, char, char, int, int);
+int print_octal(va_list, char[], int *, char, char, int, int);
+int print_hex_lower(va_list, char[], int *, char, char, int, int);
+int print_hex_upper(va_list, char[], int *, char, char, int, int);
 
 #endif
