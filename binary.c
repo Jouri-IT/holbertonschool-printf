fdba56c086d9a121da48abfd4e44b5f54e641ecc
tala2277 <tt1341700@gmail.com>
2026-04-16 17:10:28 +0000

    fix binary signature

--- binary.c
+++ binary.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
-int print_binary(va_list args, char buffer[], int *index, char flag, char length, int width)
+int print_binary(va_list args, char buffer[], int *index,
+	char flag, char length, int width, int precision)
 {
-	unsigned long int n;
-	int started;
+	unsigned int n;
 	int count;
 	int i;
+	int started;
 	int len;
-	unsigned long int tmp;
+	unsigned int tmp;
 
 	(void)flag;
-	if (length == 'l')
-		n = va_arg(args, unsigned long int);
-	else if (length == 'h')
-		n = (unsigned short)va_arg(args, unsigned int);
-	else
-		n = va_arg(args, unsigned int);
+	(void)length;
+	(void)precision;
 
+	n = va_arg(args, unsigned int);
 	len = 1;
 	tmp = n;
 	while (tmp > 1)
@@ -29,17 +27,12 @@ int print_binary(va_list args, char buffer[], int *index, char flag, char length
 	count += print_padding(width, len, buffer, index);
 
 	started = 0;
-	for (i = (sizeof(unsigned long int) * 8) - 1; i >= 0; i--)
+	for (i = 31; i >= 0; i--)
 	{
 		if ((n >> i) & 1)
-		{
 			started = 1;
-			count += buffer_char('1', buffer, index);
-		}
-		else if (started)
-		{
-			count += buffer_char('0', buffer, index);
-		}
+		if (started)
+			count += buffer_char(((n >> i) & 1) + '0', buffer, index);
 	}
 
 	if (!started)
