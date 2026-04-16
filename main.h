69a5f16992792ac3973089571f14d233b48b342f
tala2277 <tt1341700@gmail.com>
2026-04-16 14:25:11 +0000

    fix write_char prototype

--- main.h
+++ main.h
@@ -7,17 +7,24 @@
 typedef struct specifier_s
 {
 	char spec;
-	int (*handler)(va_list, char[], int *);
+	int (*handler)(va_list);
 } specifier_t;
 
 int _printf(const char *format, ...);
 
-int buffer_char(char c, char buffer[], int *index);
-int flush_buffer(char buffer[], int *index);
+int write_char(char c);
 
-int print_char(va_list args, char buffer[], int *index);
-int print_string(va_list args, char buffer[], int *index);
-int print_percent(va_list args, char buffer[], int *index);
-int print_int(va_list args, char buffer[], int *index);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
+
+int print_binary(va_list args);
+
+int print_unsigned(va_list args);
+int print_octal(va_list args);
+int print_hex_lower(va_list args);
+int print_hex_upper(va_list args);
+int print_base_unsigned(unsigned int n, char *base);
 
 #endif
